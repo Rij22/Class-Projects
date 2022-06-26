c96d06763359d389090f9d94855d1eb3d888dd86
Rij22 <98949103+Rij22@users.noreply.github.com>
2022-06-26 00:33:58 +0000

    Cmmit changes

---
+++ num.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+function find_max(nums) {
+    let max_num = Number.NEGATIVE_INFINITY;  // smaller than all other numbers;
+    for (let num of nums) {
+        if (num > max_num) {
+            // (fill in the missing line here)
+        }
+    }
+    return max_num;
+}
